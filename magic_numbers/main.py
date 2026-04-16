4476590aa1a0264a7b79a4086af03f1b3975835e
paalzoltanweb <paal.donat@gmail.com>
2026-04-16 19:39:36 +0200

    Input parsing

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,10 +1,25 @@
 from pathlib import Path
 
-
 def main():
-    data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    data = Path(Path(__file__).parent / "input.txt").read_text(encoding="utf-8")
+    input_lines = data.splitlines()
+    parsed_lines = []
 
+    for line in input_lines:
+        parsed_lines.append(parse_input_line(line))
+
+    print(parsed_lines)
+
+def parse_input_line(input: str) -> str:
+    if ("^" in input):
+        expression_parts = input.split("^")
+
+        base = int(expression_parts[0])
+        exponent = int(expression_parts[1])
+
+        return str(base**exponent)
+    
+    return input
 
 if __name__ == "__main__":
     main()
